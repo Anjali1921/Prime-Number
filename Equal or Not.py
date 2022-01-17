4527d2eabac61da883b3f54416d891bfddca1d3d
Anjali1921 <97038276+Anjali1921@users.noreply.github.com>
2022-01-17 19:19:15 +0530

    Add files via upload

---
+++ Equal or Not.py
@@ -0,0 +1,12 @@
+print("I am a maximum and minimum bot:")
+
+a = input("Enter first number:")
+b = input("Enter the second number:")
+if a>b:
+    print("a, is the maximum of the two numbers")
+    print("b, is the minimum of the two numbers")
+elif a==b:
+    print("Both are equal")
+else:
+    print("b, is the maximum of the two numbers")
+    print("a, is the minimum of the two numbers")
