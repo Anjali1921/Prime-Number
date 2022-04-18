9c63b9e12aee3e85a2e87f0010f1b5fd798f9958
Anjali1921 <97038276+Anjali1921@users.noreply.github.com>
2022-04-18 19:30:20 +0530

    Add files via upload

--- snake.py
+++ snake.py
@@ -1,18 +1,20 @@
-# importing libraries
+#importing libraries
 import turtle
 import random
 import time
 
 
-# creating turtle screen
+#creating turtle screen
 screen = turtle.Screen()
-screen.title('Mastermind- SNAKE GAME')
+screen.title('Mastermind-SNAKE GAME')
 screen.setup(width = 700, height = 700)
 screen.tracer(0)
 turtle.bgcolor('turquoise')
 
 
-## creating a border for our game
+
+##creating a border for our game
+
 turtle.speed(5)
 turtle.pensize(4)
 turtle.penup()
@@ -29,8 +31,7 @@ turtle.forward(500)
 turtle.penup()
 turtle.hideturtle()
 
-
-# score
+#score
 score = 0
 delay = 0.1
 
@@ -39,34 +40,33 @@ delay = 0.1
 snake = turtle.Turtle()
 snake.speed(0)
 snake.shape('square')
-snake.color('black')
+snake.color("black")
 snake.penup()
 snake.goto(0,0)
 snake.direction = 'stop'
 
 
-# food
-food = turtle.Turtle()
-food.speed(0)
-food.shape('circle')
-food.color('red')
-food.penup()
-food.goto(30,30)
+#food
+fruit = turtle.Turtle()
+fruit.speed(0)
+fruit.shape('circle')
+fruit.color('red')
+fruit.penup()
+fruit.goto(30,30)
 
-old_fruit = []
+old_fruit=[]
 
-
-# scoring
+#scoring
 scoring = turtle.Turtle()
 scoring.speed(0)
-scoring.color('black')
+scoring.color("black")
 scoring.penup()
 scoring.hideturtle()
 scoring.goto(0,300)
-scoring.write("Score:", align="center", font =("courier","24","bold"))
+scoring.write("Score :",align="center",font=("Courier",24,"bold"))
 
 
-# define how to move
+#######define how to move
 def snake_go_up():
     if snake.direction != "down":
         snake.direction = "up"
@@ -78,7 +78,7 @@ def snake_go_down():
 def snake_go_left():
     if snake.direction != "right":
         snake.direction = "left"
-        
+
 def snake_go_right():
     if snake.direction != "left":
         snake.direction = "right"
@@ -107,70 +107,63 @@ screen.onkeypress(snake_go_down, "Down")
 screen.onkeypress(snake_go_left, "Left")
 screen.onkeypress(snake_go_right, "Right")
 
-# Main loop
+#main loop
 
 while True:
         screen.update()
-            # snake and fruit colisions
-        if snake.distance(new_fruit)< 20:
+            #snake and fruit coliisions
+        if snake.distance(fruit)< 20:
                 x = random.randint(-290,270)
                 y = random.randint(-240,240)
-                new_fruit.goto(x,y)
+                fruit.goto(x,y)
                 scoring.clear()
                 score+=1
                 scoring.write("Score:{}".format(score),align="center",font=("Courier",24,"bold"))
                 delay-=0.001
-
-                # creating new_ball
+                
+                ## creating new_ball
                 new_fruit = turtle.Turtle()
                 new_fruit.speed(0)
                 new_fruit.shape('square')
                 new_fruit.color('red')
                 new_fruit.penup()
-                new_fruit.append(new_fruit)
+                old_fruit.append(new_fruit)
+                
 
-
-        # adding ball to snake
-
+        #adding ball to snake
+        
         for index in range(len(old_fruit)-1,0,-1):
                 a = old_fruit[index-1].xcor()
                 b = old_fruit[index-1].ycor()
 
                 old_fruit[index].goto(a,b)
-
+                                     
         if len(old_fruit)>0:
-                a = snake.xcor()
+                a= snake.xcor()
                 b = snake.ycor()
                 old_fruit[0].goto(a,b)
         snake_move()
 
-        # snake and border collision
-        if snake.xcor()>280 or snake.xcor()< -300 or snake.ycor()>240 or snake.ycor()< -240:
+        ##snake and border collision    
+        if snake.xcor()>280 or snake.xcor()< -300 or snake.ycor()>240 or snake.ycor()<-240:
                 time.sleep(1)
                 screen.clear()
                 screen.bgcolor('turquoise')
                 scoring.goto(0,0)
-                scoring.write(' GAME OVER \n Your Score is {}'.format(score),align="center",font=("courier",30,"bold"))
+                scoring.write("   GAME OVER \n Your Score is {}".format(score),align="center",font=("Courier",30,"bold"))
 
 
-        # snake collision
+        ## snake collision
         for food in old_fruit:
                 if food.distance(snake) < 20:
                         time.sleep(1)
                         screen.clear()
                         screen.bgcolor('turquoise')
                         scoring.goto(0,0)
-                        scoring.write(' GAME OVER \n Your Score is {}'.format(score),align="center",font=("courier",30,"bold"))
+                        scoring.write("    GAME OVER \n Your Score is {}".format(score),align="center",font=("Courier",30,"bold"))
 
-        
-        
+
+                
         time.sleep(delay)
 
 turtle.Terminator()
-
-
-
-
-
-
-
